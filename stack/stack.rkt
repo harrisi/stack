70c23269c9cb637e65787d45cf63769b0ecc3819
Ian Harris <ian+gh@dgby.org>
2016-03-03 23:55:00 -0800

    Implemented arbitrary functions
    Program will now run any arbitrary function, as long as it is a
    binary function.

--- stack/stack.rkt
+++ stack/stack.rkt
@@ -1,20 +1,49 @@
 #lang racket
 
-;; (require racket/cmdline)
-
-
 (define (push elem [stack null])
   (cons elem stack))
 
 (define (pop stack)
-  (if (null? stack)
-      '()
-      (rest stack)))
+  (unless (empty? stack)
+    (rest stack)))
 
 (define (peek stack)
-  (if (null? stack)
-      '()
-      (first stack)))
+  (unless (null? stack)
+    (first stack)))
 
 (define (run prog)
-  (eval (foldl cons null prog)))
+  (define stack null)
+  (for ([iprog prog])
+    (if (procedure? iprog)
+        (let ([num1 (peek stack)]
+              [num2 (peek (pop stack))])
+          (set! stack (push (iprog num1 num2) (pop (pop stack)))))
+        (set! stack (push iprog stack))))
+  (println stack))
+
+(define (run_old prog)
+  (define stack null)
+  (for ([iprog prog])
+    (cond
+      ;;  Token = +
+      ([eq? iprog '+]
+       (let ([num1 (peek stack)]
+             [num2 (peek (pop stack))])
+         (set! stack (push (+ num1 num2) (pop (pop stack))))))
+      
+      ;; Token = *
+      ([eq? iprog '*]
+       (let ([num1 (peek stack)]
+             [num2 (peek (pop stack))])
+         (set! stack (push (* num1 num2) (pop (pop stack))))))
+      
+      ;; Token = -
+      ([eq? iprog '-]
+       (let ([num1 (peek stack)]
+             [num2 (peek (pop stack))])
+         (set! stack (push (- num1 num2) (pop (pop stack))))))
+      
+      ;; Token is literal
+      (else
+       (set! stack (push iprog stack)))))
+  (println stack))
