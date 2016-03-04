7306c604bfbd89a03bc13cdfdffd3dbf3e19e207
Ian Harris <ian+gh@dgby.org>
2016-03-04 00:20:21 -0800

    added some documentation

--- stack.rkt
+++ stack.rkt
@@ -1,49 +1,72 @@
 #lang racket
 
+;; Push elem onto stack (default value null).
 (define (push elem [stack null])
   (cons elem stack))
 
+;; Pop item off stack, returning the stack after pop.
 (define (pop stack)
   (unless (empty? stack)
     (rest stack)))
 
+;; Peek at top element of stack, returning it.
 (define (peek stack)
   (unless (null? stack)
     (first stack)))
 
+;; Run a program defined as a stack.
 (define (run prog)
+  ;; Internal stack.
   (define stack null)
-  (for ([iprog prog])
-    (if (procedure? iprog)
+  ;; for each element in program...
+  (for ([elem prog])
+    ;; if current element is a procedure..
+    (if (procedure? elem)
+        ;; local bindings for the top two elements..
         (let ([num1 (peek stack)]
               [num2 (peek (pop stack))])
-          (set! stack (push (iprog num1 num2) (pop (pop stack)))))
-        (set! stack (push iprog stack))))
-  (println stack))
+          ;; set! the internal stack to be the current stack with top two
+          ;; elements popped off and the result of applying current elem to them
+          ;; pushed to stack.
+          (set! stack (push (elem num1 num2) (pop (pop stack)))))
+        ;; Otherwise, set! the internal stack to the result of pushing the
+        ;; current element to the stack.
+        (set! stack (push elem stack))))
+  stack)
 
+;; Old (start) of implementing run with cond. This is nice because there's not
+;; arbitrary code execution, but it makes adding functionality more work.
+;; I might continue to update this with "internal" functions as a "safe" option
+;; to go alongside the above "wild west" implementation.
 (define (run_old prog)
   (define stack null)
-  (for ([iprog prog])
+  (for ([elem prog])
     (cond
       ;;  Token = +
-      ([eq? iprog '+]
+      ([eq? elem '+]
        (let ([num1 (peek stack)]
              [num2 (peek (pop stack))])
          (set! stack (push (+ num1 num2) (pop (pop stack))))))
       
       ;; Token = *
-      ([eq? iprog '*]
+      ([eq? elem '*]
        (let ([num1 (peek stack)]
              [num2 (peek (pop stack))])
          (set! stack (push (* num1 num2) (pop (pop stack))))))
       
       ;; Token = -
-      ([eq? iprog '-]
+      ([eq? elem '-]
        (let ([num1 (peek stack)]
              [num2 (peek (pop stack))])
          (set! stack (push (- num1 num2) (pop (pop stack))))))
+
+      ;; Token = /
+      ([eq? elem '/]
+       (let ([num1 (peek stack)]
+             [num2 (peek (pop stack))])
+         (set! stack (push (/ num1 num2) (pop (pop stack))))))
       
       ;; Token is literal
       (else
-       (set! stack (push iprog stack)))))
-  (println stack))
+       (set! stack (push elem stack)))))
+  stack)
